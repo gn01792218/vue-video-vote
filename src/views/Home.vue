c0e0274626bfa81675497e6d59160f3b272f7eb2
MSI\gn01792218 <gn01792218@hotmail.com>
2023-12-04 16:15:05 +0800

    [feature] add lotteryModal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,19 @@
   <main class="h-screen">
     <section class="h-[10%] bg-slate-500 flex justify-center items-center">
       <h1 class="text-5xl">XX公司尾牙大樂透</h1>
-      <button class="border-2" @click="play">抽獎</button>
+      <button type="button" class="border-2" @click="play">抽獎</button>
     </section>
     <section class="flex w-100 h-[90%]">
       <aside class="w-[10%] h-full">
-        <LotteryList
-          :lottery-list="lotteryList"
-        />
+        <LotteryList :lottery-list="lotteryList" />
       </aside>
       <section class="bg-slate-600 w-[90%] h-full flex">
-        <UserListBar
-          :class="[`w-[${100/userBarCols}%]`]"
-          v-for="index in userBarCols" :key="index"
-          :rows="userBarRows"
-          :user-list="userList.slice(userBarRows*index,userBarRows*index+userBarRows)"
-        />
+        <UserListBar :class="[`w-[${100 / userBarCols}%]`]" v-for="index in userBarCols" :key="index" :rows="userBarRows"
+          :user-list="userList.slice(userBarRows * index, userBarRows * index + userBarRows)" />
       </section>
     </section>
   </main>
+  <LotteryModal />
 </template>
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
@@ -27,6 +22,16 @@ import { useLotteryStore } from '@/store/useLotteryStore';
 import { useUserStore } from '@/store/useUserStore'
 import UserListBar from '@/component/UserListBar.vue'
 import LotteryList from "@/component/LotteryItemList.vue"
+import LotteryModal from '@/component/LotteryModal.vue';
+import {
+  Modal,
+  Ripple,
+  initTE,
+} from "tw-elements";
+
+onMounted(() => {
+  // initTE({ Modal, Ripple });
+})
 
 //Pinia
 const { lotteryList } = storeToRefs(useLotteryStore())
@@ -38,13 +43,14 @@ const { loadUserList } = useUserStore()
 const userBarCols = 20
 const userBarRows = 10
 //初始化
-for(let i = 0 ; i<10; i++ ){
+for (let i = 0; i < 10; i++) {
   loadUserList()
 }
 loadLotteryList()
 
 //function
-function play(){
-  alert('抽獎')
+function play() {
+  const myModal = new Modal(document.getElementById("lotteryModal"));
+  myModal.show()
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 }
