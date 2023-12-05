6dd10bd39e3a1fc01fdaee2af9eb4c3ba828ecb9
MSI\gn01792218 <gn01792218@hotmail.com>
2023-12-05 11:22:33 +0800

    [fix] fix deploy bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,18 @@
   <main class="h-screen">
     <section class="h-[10%] bg-slate-500 flex justify-center items-center">
       <h1 class="text-5xl">XX公司尾牙大樂透</h1>
-      <button type="button" class="w-[100px] h-[50px] border-2 animate-bounce border-blue-500 bg-blue-800 ml-5 text-white rounded-xl" @click="play">抽獎</button>
+      <button type="button"
+        class="w-[100px] h-[50px] border-2 animate-bounce border-blue-500 bg-blue-800 ml-5 text-white rounded-xl"
+        @click="play">抽獎</button>
     </section>
     <section class="flex w-full h-[90%]">
       <aside class="w-[10%] h-full">
         <LotteryList :lottery-list="lotteryList" />
       </aside>
       <section class="bg-slate-600 w-[90%] h-full flex">
-        <UserListBar v-for="index in userBarCols" :key="index" :rows="userBarRows"
-          :user-list="userList.slice(userBarRows * index, userBarRows * index + userBarRows)" 
-          :style='`width: ${100/userBarCols}%;`'
-          />
+        <UserListBar :id="`userBar-${index}`" v-for="index in userBarCols" :key="index" :rows="userBarRows"
+          :user-list="userList.slice(userBarRows * index, userBarRows * index + userBarRows)"
+          :style='`width: ${100 / userBarCols}%;`' />
       </section>
     </section>
   </main>
@@ -26,6 +27,7 @@ import UserListBar from '@/component/UserListBar.vue'
 import LotteryList from "@/component/LotteryItemList.vue"
 import LotteryModal from '@/component/LotteryModal.vue';
 import {
+  Animate,
   Modal,
 } from "tw-elements";
 
@@ -48,5 +50,15 @@ loadLotteryList()
 function play() {
   const myModal = new Modal(document.getElementById("lotteryModal"));
   myModal.show()
+  for (let i = 0; i < userBarCols; i++) {
+    const evenAnimationName = ''
+    const oddAnimationName = ''
+    new Animate(document.getElementById(`userBar-${i}`), {
+      animation: i % 2 === 0 ? evenAnimationName : oddAnimationName,
+      animationReset:true,
+      animationInterval:0
+    })
+  }
+
 }
 </script>
