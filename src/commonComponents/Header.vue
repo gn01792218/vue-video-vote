1e753f5d7530c2a4374098b4290672abcc5893bc
MSI\gn01792218 <gn01792218@hotmail.com>
2023-12-05 10:34:54 +0800

    [fix] fix build bug

--- src/commonComponents/Header.vue
+++ src/commonComponents/Header.vue
@@ -4,9 +4,6 @@
             <li class="cursor-pointer mr-2">
                 <RouterLink to="/">首頁</RouterLink>
             </li>
-            <li class="cursor-pointer">
-                <RouterLink to="/About">測試路由</RouterLink>
-            </li>
         </ul>
     </nav>
 </template>
